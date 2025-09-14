7ead19bba58268f3b3cf471a823899c6e9283ca3
Kareem Yasser <kareem.yasserr@gmail.com>
2025-09-14 16:44:38 +0300

    feat: add package.json and testing file

---
+++ src/test.sh
@@ -0,0 +1,11 @@
+EXPECTED="Hello, Test!"
+
+OUTPUT=$(node -e "console.log(require('./src/app.js').greet('Test'))")
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+    echo "✅ Test passed!"
+    exit 0
+else
+    echo "❌ Test failed Expected: $EXPECTED, but got: $OUTPUT"
+    exit 1
+fi
